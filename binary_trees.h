d13c44b462ef8ffe229f6e20db4ec6a841fa14b2
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-11 19:45:52 +0000

    Header

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -27,32 +27,9 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /** Functions **/
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
-void binary_tree_delete(binary_tree_t *tree);
-
-
-
-/** Structure and prototype of print function*/
-#include <stddef.h>
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);/** 0 **/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);/** 1 **/
+void binary_tree_delete(binary_tree_t *tree);/** 3 **/
 
 #endif /* BINARY_TREES_H */
