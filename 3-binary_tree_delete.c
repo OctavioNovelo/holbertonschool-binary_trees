01d371abb7bab10bfdb0ec21d4204df4ba67ea7d
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-11 20:18:03 +0000

    Task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_delete - delete a binary tree 
+ *binary_tree_delete - delete a binary trees
  *@tree: pointer to root node
  */
 void binary_tree_delete(binary_tree_t *tree)
