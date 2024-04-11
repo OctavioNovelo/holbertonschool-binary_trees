fe396b77a6673c127c048bd5e6b5d3c3b3d187b5
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-11 18:54:26 +0000

    Header1

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,5 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -22,8 +21,12 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/** Structure and prototype of print function*/
+/** Functions **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
+
+/** Structure and prototype of print function*/
 #include <stddef.h>
 
 /**
