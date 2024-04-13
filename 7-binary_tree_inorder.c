07b5b58d92b3ec96aec15863044176051a750fb7
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-13 01:05:28 +0000

    Corrected name of header file

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,4 +1,4 @@
-#include "binary-trees.h"
+#include "binary_trees.h"
 /**
  *binary_tree_inorder - a function that goes through a binary tree using in-order traversal
  *@tree: a pointer to the root node
