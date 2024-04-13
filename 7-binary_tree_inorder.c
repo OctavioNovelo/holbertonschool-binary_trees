48472f3f47a636084a0721cf249cf74c8b20c1fd
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-13 01:08:42 +0000

    Corrected maximum lenght of documentation

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,13 @@
 #include "binary_trees.h"
+
 /**
- *binary_tree_inorder - a function that goes through a binary tree using in-order traversal
- *@tree: a pointer to the root node
- *@func: a pointer to a function to call for each node
- *Return: If tree or func is NULL do nothing
+ * binary_tree_inorder - a function that goes through a binary tree using
+ * in-order traversal
+ * @tree: a pointer to the root node
+ * @func: a pointer to a function to call for each node
+ * Return: If tree or func is NULL do nothing
  */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (tree == NULL || func == NULL)
