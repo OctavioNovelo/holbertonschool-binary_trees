6867a5c6cda6b3b48e9d397143dee998f6a38a9a
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-13 17:34:04 +0000

    Task15

--- binary_trees.h
+++ binary_trees.h
@@ -42,6 +42,7 @@ size_t binary_tree_size(const binary_tree_t *tree);/** 11 **/
 size_t binary_tree_leaves(const binary_tree_t *tree);/** 12 **/
 size_t binary_tree_nodes(const binary_tree_t *tree);/** 13 **/
 int binary_tree_balance(const binary_tree_t *tree);/** 14 **/
+int binary_tree_is_full(const binary_tree_t *tree);/** 15 **/
 int binary_tree_is_perfect(const binary_tree_t *tree);/** 16 **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);/** 18 **/
 #endif /* BINARY_TREES_H */
