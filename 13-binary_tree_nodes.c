5513618a9ed32b1c16560feddefc5315029e573d
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-13 17:19:57 +0000

    Task13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_nodes - counts the nodes
+ *@tree: a pointer to the root node
+ *
+ *Return: 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -40,6 +40,7 @@ size_t binary_tree_height(const binary_tree_t *tree);/** 9 **/
 size_t binary_tree_depth(const binary_tree_t *tree);/** 10 **/
 size_t binary_tree_size(const binary_tree_t *tree);/** 11 **/
 size_t binary_tree_leaves(const binary_tree_t *tree);/** 12 **/
+size_t binary_tree_nodes(const binary_tree_t *tree);/** 13 **/
 int binary_tree_balance(const binary_tree_t *tree);/** 14 **/
 int binary_tree_is_perfect(const binary_tree_t *tree);/** 16 **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);/** 18 **/
