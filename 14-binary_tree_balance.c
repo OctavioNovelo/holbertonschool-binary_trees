11a924dd8ede79e2ec253c2874424ab4c39239b0
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-13 02:27:19 +0000

    Testing code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,8 +29,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left) : 1;
-	right_height = binary_tree_height(tree->right) : 1;
+	left_height = binary_tree_height(tree->left); : 1
+	right_height = binary_tree_height(tree->right); : 1
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
