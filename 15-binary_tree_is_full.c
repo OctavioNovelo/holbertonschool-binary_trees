06abd0577c522be34e14b5ea00fded2d7a29610f
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-14 00:06:43 +0000

    Task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,5 +11,5 @@ if (tree == NULL)
 return (0);
 if ((tree->left && tree->right == NULL) || (tree->left == NULL && tree->right))
 return (0);
-return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+return (1);
 }
